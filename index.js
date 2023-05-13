b99c1ea0ebd5ae4ec0bed39ec3e1567a1b446562
thiagolordello <thiagolordello@hotmail.com>
2023-05-13 15:21:41 -0300

    remove all

--- index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app');
-
-app.listen(3001, () => {
-  console.log('Escutando na porta 3001');
-});
-
--- routers/registerRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-const controllers = require('../controllers');
-
-router.post('/', controllers.registerController);
-
-module.exports = router;
