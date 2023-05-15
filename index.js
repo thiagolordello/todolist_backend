f079f36c0eaf657e89c67d22662a463e7ed840c0
thiagolordello <thiago.lordello@synoro.com.br>
2023-05-15 13:11:15 -0300

    refatora var de amb

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-require('dotenv').config();
 const app = require('./app');
+
 const host = process.env.DB_HOST;
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
-
 const port = process.env.PORT || 3000;
 
 app.listen(port,"0.0.0.0", () => {
   console.log('Escutando na porta 3001');
 });
 
+
+
+
 // Use as variáveis conforme necessário no seu código
 console.log(`Conectando ao banco de dados em ${host} com usuário ${user} e senha ${password}`);
-
-
-
 
 
