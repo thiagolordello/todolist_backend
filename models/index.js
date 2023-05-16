21d97368c5392fc57170ed280915d7be25f4f078
thiagolordello <thiago.lordello@synoro.com.br>
2023-05-16 18:29:03 -0300

    index2

--- models/index.js
+++ models/index.js
@@ -1,14 +1,17 @@
+
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.json`)[env];
 const db = {};
 
 const databaseUrl = process.env.MYSQL_URL;
-const sequelize = new Sequelize(databaseUrl, config);
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: 'mysql',
+
+}); 
 
 fs
   .readdirSync(__dirname)
@@ -28,4 +31,5 @@ db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+
 
